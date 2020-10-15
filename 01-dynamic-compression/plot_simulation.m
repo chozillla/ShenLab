6698077bf22f680ee508464bb57eecba239916a5
Chemay Shola <chemay.shola@gmail.com>
2020-10-15 16:21:35 -0700

    Add files via upload

--- 01-dynamic-compression/plot_simulation.m
+++ 01-dynamic-compression/plot_simulation.m
@@ -1,41 +1,46 @@
 %Plots the simulations 
 %% Completely Random 
+%Actually this is non-random just too lazy to relabel
 
 n_init_trials = 20;
 sim_res_err_ran = [];
-parfor trials = 1:40
+parfor trials = 1:100
     sim_res_err_ran(:,trials) = simulation(n_init_trials);
 end
 %values 
-avg_error_ran = mean(sim_res_err_ran,2);
+avg_error_ran = mean(sim_res_err_ran,2)';
 std_error_ran = std(sim_res_err_ran);
 
 %% Greater than 20 i.e not random
 
-n_init_trials = 40;
+n_init_trials = 100;
 sim_res_err = [];
-parfor trials = 1:40
+parfor trials = 1:100
     sim_res_err(:,trials) = simulation(n_init_trials);
 end
 %values 
-avg_error = mean(sim_res_err,2);
+avg_error = mean(sim_res_err,2)';
 std_error = std(sim_res_err);
 
 %% Plot
-alpha = 0.05;
-standard_error_ran = std(avg_error_ran)*ones(size(trials));
-standard_error = std(avg_error)*ones(size(trials));
+% alpha = 0.05;
+% trials = linspace(1,100)';
+% standard_error_ran = std(avg_error_ran)*ones(size(trials));
+% standard_error = std(avg_error)*ones(size(trials));
 
-trials = linspace(1,100)';
+figure(1)
+amatrix = sim_res_err_ran';
+stdshade(amatrix,0.5,'r')
+xlabel('Trials');
+ylabel('Error');
+legend('Iterative','Random');
+hold on
+amatrix = sim_res_err';
+stdshade(amatrix,0.5,'g')
+
+
 
 %Plot
-figure(1)
-confplot(trials,avg_error,standard_error);
-figure(2)
-confplot(trials,avg_error_ran,standard_error_ran);
-
-L = findobj(1,'type','line');
-copyobj(L,findobj(2,'type','axes'));
 
 % figure(1)
 % plot(avg_error_ran,'k')
