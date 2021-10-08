2ba44afdcae5e9ae67dd2b6deafa34ec5598d079
Chemay Shola <chemay.shola@gmail.com>
2021-10-07 22:21:15 -0700

    Add files via upload

--- dragpoints_2_v1.m
+++ dragpoints_2_v1.m
@@ -1,31 +1,31 @@
 function dragpoints_2_v1(xData,yData,xLower,xUpper,yLower,yUpper)
 global openmha
-global x
-global y
-global num_pins % Number of pins
-global point_count % Number of datapoints from the cursor 
-global points
-global area 
-global handles
-global trial_count 
+global x % X coordinate
+global y % Y coordinate
+global num_pins % number of pins
+global point_count % number of datapoints from the cursor 
+global points % X and Y coordinate pair per each movement of the cursor
+global handles % handle structure that stores the "Pin" and "Next" button states 
+global trial_count % number of trials, which is counted when the user clicks the next pin 
 
-area = (1/3)*(20*20); % Total area of the screen
-assignin('base','points',points)
+assignin('base','points',points) % saving this variable to the workspace for easy viewing
 num_pins = 0;
 point_count = 0;
-points = zeros(1000,3); % First column is x and the second is y third column is the weight
+points = zeros(1000,3); % First column is x and the second is y third column is the weight of the coordinate pair
+                        % dropping a pin is a weight of 1 and clicking next
+                        % is a weight of 2 
 trial_count = 0; % Number of trials 
 
 %Setting Up The Correct Directories 
 
-setenv('PATH', [getenv('PATH') ':/usr/local/bin']);         
-addpath('/usr/local/lib/openmha/mfiles/')
-javaaddpath('/usr/local/lib/openmha/mfiles/mhactl_java.jar')
+setenv('PATH', [getenv('PATH') ':/usr/local/bin']); % Make sure to change this to your appropiate directory        
+addpath('/usr/local/lib/openmha/mfiles/') % Make sure to change this to your appropiate directory        
+javaaddpath('/usr/local/lib/openmha/mfiles/mhactl_java.jar') % Make sure to change this to your appropiate directory        
 
 openmha = mha_start;       %Starts openMHA software
 mha_query(openmha,'','read:final_dc_live.cfg'); %Selects the .cfg file to read
 
-if nargin == 0 
+if nargin == 0 % default input variables
   xData = 0;
   yData = 0;
   xLower = -20;
@@ -34,22 +34,22 @@ if nargin == 0
   yUpper = 20;
 end
 
-figure('unit','normalized',...
+figure('unit','normalized',...  % creates the MATLAB figure
     'position',[.1 .1 .8 .8]);
 
-handles.hbutton1 = uicontrol('style','pushbutton',...,
+handles.hbutton1 = uicontrol('style','pushbutton',...,  % creates the "Pin" button with the various properties
     'unit','normalized',...,
     'innerposition',[0.02 0.55 .08 .08],'fontname','Arial',...
     'fontsize',36,'backgroundcolor','green','string','Pin','Tag','1');
-handles.hbutton2 = uicontrol('style','pushbutton',...,
+handles.hbutton2 = uicontrol('style','pushbutton',...,  % creates the "Next" button with the various properties
     'unit','normalized',...,
     'innerposition',[0.02 0.25 .08 .08],'fontname','Arial',...
     'fontsize',36,'backgroundcolor','blue','string','Next','Tag','2','Enable','Off');
 
-hObject1 = handles.hbutton1;
-hObject2 = handles.hbutton2;
+hObject1 = handles.hbutton1;    % creates the button object
+hObject2 = handles.hbutton2;    % creates the button object
 
-guidata(hObject1, handles);
+guidata(hObject1, handles);     
 guidata(hObject2, handles);
 
 x = xData;
@@ -59,7 +59,7 @@ ax = axes('xlimmode','manual','ylimmode','manual');
 ax.XLim = [xLower xUpper];
 ax.YLim = [yLower yUpper];
 
-handles.hbutton1.Callback = {@button_callback, handles};
+handles.hbutton1.Callback = {@button_callback, handles}; 
 handles.hbutton2.Callback = {@button_callback, handles};
 line(x,y,'color','c','marker','.','markersize',105,'hittest','on','buttondownfcn',@clickmarker) %Change this with rectangle that's filled 
 
@@ -70,40 +70,41 @@ global x
 global y
 global num_pins
 global area_explored
-global area
 global points
 global point_count
 global trial_count
 
-buttonID = src.Tag;              %Sets the tag for the buttons 
-stateII = str2num(buttonID);    %Converts the tag to a state 
+surface_area = (1/3)*(20*20); % Total area of the screen
 
-if (num_pins == 3) || (area_explored > area)
-     %h = findall(gcf,'Type','line');
+%This calculates the area explored by the user
+x_range = range(points(:,1));   
+y_range = range(points(:,2));
+area_explored = x_range * y_range;  % area explored by the user
+buttonID = src.Tag;              %Sets the tag for the buttons 
+stateII = str2double(buttonID);    %Converts the tag to a state 
+
+%if (num_pins == 3) || (area_explored > area)
+if ((num_pins == 3)||(area_explored > surface_area))
+     disp(area_explored)
      h = findall(gca,'Type','rectangle'); % use a tag to search for each pin 
      delete(h) % Removes all the pins
-     %set(h,'Visible','off');
-     %delete(handles.circle)
-     handles.hbutton1.Enable = 'off';
-     handles.hbutton2.Enable = 'on';
+     handles.hbutton1.Enable = 'off'; % "Pin" button
+     handles.hbutton2.Enable = 'on'; % "Next" button
      pause(5/1000)
      points(point_count,3) = 2;
      trial_count = trial_count +1;
      num_pins = 0;
      area_explored = 0;
-     disp(num_pins)
+     disp("trial")
 else
     if  stateII == 1 % Dropping a pin 
-        disp(stateII)
+        disp("pin")
         xy = get(gca,'CurrentPoint');
         x = xy(1,1);
         y = xy(1,2);
-        %location = [x y]
         num_pins = num_pins + 1;
-        %handles.circle{count} = viscircles([x,y], 2); % Use annotation or
-        %rectangle to drop a circle
         %radius
-        r = 1;
+        r = 2;
         c = [x y];
         pos = [c-r 2*r 2*r];
         rectangle('Position',pos,'Curvature',[1 1],'EdgeColor','r')
@@ -115,11 +116,15 @@ else
         points(point_count,3) = 1;
         drawnow limitrate
     elseif stateII == 2 % Next button
-        handles.hbutton1.Enable = 'on';
-        handles.hbutton2.Enable = 'off';
+        handles.hbutton1.Enable = 'on'; % "Pin" button
+        handles.hbutton2.Enable = 'off'; % "Next" button
         num_pins = 0;
-        Y = [num_pins 'Next_button'];
-        disp(Y)
+        area_explored = 0;
+        %moves the cursor to the origin
+        h = findall(gca,'Type','line'); %this is the cursor 
+        set(h,'XData',0)    % setting the x coordinate to 0
+        set(h,'Ydata',0)    % setting the y coordinate to 0 
+        disp("next")
     end
     drawnow
 end
@@ -133,14 +138,12 @@ global x
 global y
 global points
 global point_count
-global area_explored
-
-%disp("drag marker")
+%global area_explored
 
 %This calculates the area explored by the user
-x_range = range(points(:,1));
-y_range = range(points(:,2));
-area_explored = x_range * y_range;
+% x_range = range(points(:,1));   
+% y_range = range(points(:,2));
+% area_explored = x_range * y_range;  % area explored by the user
 
 %get current axes and coords
 %h2 = findall(groot,'Type','Axes');
