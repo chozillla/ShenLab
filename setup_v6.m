392f83b02bbd6e5eb8a0ae818f1f5757bb1ec9a2
Chemay Shola <chemay.shola@gmail.com>
2021-10-22 17:48:00 -0700

    Add files via upload

---
+++ setup_v6.m
@@ -0,0 +1,32 @@
+global x % X coordinate
+global y % Y coordinate
+global num_pins % number of pins
+global pin_count % count of pins 
+global data % data structure that holds all the recorded data
+global trial_count % number of trials, which is counted when the user clicks the next pin 
+global a % intialize random weight
+global Glast % the gain in 6 frequency bands of the previous trial 
+global D 
+
+num_trials = 53;
+data = cell(6,1,(num_trials));
+
+x = 0;
+y = 0;
+% preallocating where the column are the gaintable (gt) values and the rows are the trials
+a = randn(6,1); % weight for each frequency bands
+% here the 3d cell is being created
+Glast = zeros(6,1); % previous trial gain values, but this is set to be zero
+D = {}; % storing the glast 
+data{1,1,1} = a; % weight for each frequency band
+data{2,1,1} = Glast; % previous gain 
+data{3,1,1} = x; % x coordinate
+data{4,1,1} = y; % y coordinate
+data{5,1,1} = 0; % weight
+data{6,1,1} = D; % storing the previous 
+num_pins = 0;
+trial_count = 0; % Number of trials 
+pin_count = 1; % total number of pins used 
+assignin('base','data',data);
+
+dragpoints_2_v6()
